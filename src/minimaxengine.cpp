db3cda8d4d9f02bb360d87f304502580319dfdb8
Jason Christian H <nakamarujc@gmail.com>
2021-04-06 22:56:25 +0700

    Optimized minimax engine recursion by removing CoorPoint struct, storing board reference, current board copying

--- src/minimaxengine.cpp
+++ src/minimaxengine.cpp
@@ -7,122 +7,102 @@
 #include <chrono>
 
 #define RECURDEPTH 3
-#define MAXSCORE int
+#define SCORE int
 
 namespace oth {
     class MinimaxEngine : public Engine {
 
 private:
         
-        // Struct to store the point combination combo.
-        struct CoorPoint {
-            Point coor;
-            int score;
-            
-            CoorPoint() {coor = Point(); score = 0;}
-            CoorPoint(Point coor, int score) : coor(coor) , score(score) {}
-        };
-
         // Color of the tile that we would like to calculate for, and win
         Color winColor;
         int movesForeseen;
         Point chosenSquare;
 
+        // Store board reference
+        Othello* board;
+
+
         // Utility to do minimax
-        // board: othello board reference
         // curDepth: the current depth of the recursion
         // isMaxing: whether the current step is maximizing or minimizing
-        CoorPoint minimaxRecur(Othello& board, int curDepth, bool isMaxing) {
+        SCORE minimaxRecur(int curDepth, bool isMaxing) {
+            movesForeseen++;
 
             // Switch the turn every time minimax is called.
-            board.switchTurn();
+            board->switchTurn();
             
             // When recursion have reached max depth
             if (curDepth == RECURDEPTH) {
-                // Gets the coordinate for the current move
-                Point point = board.undos.top().begin()->coor;
-
                 // Score at the current step
-                int score = board.getScore(winColor);
+                int score = board->getScore(winColor);
 
                 // Don't forget to undo the move when returning something.
-                board.undoMove();
-                return CoorPoint(point, score);
+                board->undoMove();
+                return score;
             }
 
             // Choose the moves depending on the current color.
-            const std::list<Point>* move;
-            switch(board.turn) {
+            std::list<Point> move;
+            switch(board->turn) {
                 case (white):
-                    move = &board.whiteMove;
+                    move = board->whiteMove;
                     break;
                 case (black):
-                    move = &board.blackMove;
+                    move = board->blackMove;
                     break;
             }
 
-            // Create an array to contain all the CoorPoint values
-            const int validMoves = move->size();
-            CoorPoint* vals = new CoorPoint[validMoves];
+            // Create an array to contain all the score values
+            const int validMoves = move.size();
+            SCORE* vals = new SCORE[validMoves];
             int valI = 0;
 
-            // We need to copy the movelist
-            std::list<Point> moveCopied;
-            for (std::list<Point>::const_iterator it = move->begin(); it != move->end(); ++it)
-                moveCopied.push_back(*it);
-
             // Iterate every of the current move
-            for (std::list<Point>::const_iterator it = moveCopied.begin(); it != moveCopied.end(); ++it) {
-                movesForeseen++;
-
+            for (std::list<Point>::const_iterator it = move.begin(); it != move.end(); ++it) {
                 // Play the piece, as if playing self
                 // Dont forget to enable undoing
-                board.playPiece(board.turn, (*it).x, (*it).y, true);
+                board->playPiece(board->turn, (*it).x, (*it).y, true);
 
                 // Recur, depending when we want to maximize or minimize
-                vals[valI++] = minimaxRecur(board, curDepth+1, !isMaxing);
+                vals[valI++] = minimaxRecur(curDepth+1, !isMaxing);
             }
 
-            CoorPoint minMaxValue;
+            SCORE minMaxValue;
             // Check if there is any valid moves
             if (validMoves > 0) {
                 
                 // Store pointer temporarily
                 // Get the minimum / maximum depending on the state.
-                CoorPoint* tempVal = isMaxing ? 
-                    std::max_element(vals, vals+validMoves-1, [](CoorPoint a, CoorPoint b)  { return a.score < b.score; }) :
-                    std::min_element(vals, vals+validMoves-1, [](CoorPoint a, CoorPoint b)  { return a.score < b.score; });
+                SCORE* tempVal = isMaxing ? 
+                    std::max_element(vals, vals+validMoves-1):
+                    std::min_element(vals, vals+validMoves-1);
 
                 // Decide on where to go
                 if (curDepth == 0) {
-                    // Do some pointer maths to get the list.
-                    int index = tempVal - vals;
 
                     // Choose from value
-                    std::list<Point>::const_iterator it = moveCopied.begin();
-                    std::advance(it, index);
-
+                    std::list<Point>::const_iterator it = move.begin();
+                    // Do some pointer maths to get the list.
+                    std::advance(it, tempVal - vals);
                     chosenSquare = *(it);
                 }
-
+                // Assign
                 minMaxValue = *tempVal;
 
             } else {
                 // If there is no valid moves,
                 // Return min or max value.
-                Point point = board.undos.top().begin()->coor;
-                if (isMaxing) {
-                    minMaxValue = CoorPoint(point, std::numeric_limits<MAXSCORE>::max());
-                } else {
-                    minMaxValue = CoorPoint(point, std::numeric_limits<MAXSCORE>::min());
-                }
+                minMaxValue = isMaxing ?
+                    std::numeric_limits<SCORE>::max():
+                    std::numeric_limits<SCORE>::min();
             }
 
             // Free the memory
             delete[] vals;
 
             // Don't forget to undo the move when returning something.
-            board.undoMove();
+            board->undoMove();
 
             // Return
             return minMaxValue;
@@ -131,8 +111,12 @@ private:
 public:
 
         Point nextMove(Othello& board) {
-
+            
+            // Init
             movesForeseen = 0;
+
+            // Set board reference
+            this->board = &board;
 
             // Set wincolor
             winColor = board.turn;
@@ -145,7 +129,7 @@ public:
             auto start = std::chrono::high_resolution_clock::now();
 
             // Gets the coordinate
-            minimaxRecur(board, 0, true);
+            minimaxRecur(0, true);
 
             // End time
             auto end = std::chrono::high_resolution_clock::now();
